7903abec627d58f66d6026a742e1ce72c66e3ab5
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-10-14 22:49:07 +0600

    feat: create search function
    - write unit test for search func

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-use std::{fs, error::Error};
-
+use std::{error::Error, fs};
 
 #[derive(Debug)]
 pub struct Config {
@@ -10,7 +9,7 @@ pub struct Config {
 impl Config {
     pub fn build(args: &[String]) -> Result<Self, &'static str> {
         if args.len() < 3 {
-            return Err("Too few arguments") //&format!("Expected two arguments. Got {}", args.len() -1 ));
+            return Err("Too few arguments"); //&format!("Expected two arguments. Got {}", args.len() -1 ));
         }
         return Ok(Self {
             query: args[1].clone(),
@@ -27,3 +26,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res: Vec<&str> = Vec::new();
+	for line in contents.lines() {
+		if line.contains(query) {
+			res.push(line);
+		}
+	}
+	return res
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn search_test() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
