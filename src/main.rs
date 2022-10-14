2044f5e5e135fab1ac2e1625107ec523e3e01bf8
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-10-14 22:16:59 +0600

    feat: implement config logic in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-use std::env;
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, path) = parse_args(&args);
-    dbg!(query, path);
+	let config = match Config::build(&args) {
+		Ok(t) => t,
+		Err(e) => {
+			println!("Error when parsing arguments. Error: {}", e);
+			process::exit(1);
+		}
+	};
+
+    dbg!(&config, &config.query);
+    let file = match fs::read_to_string(&config.path) {
+        Ok(some) => Some(some),
+        Err(e) => panic!("Error when opening file. {}", e),
+    };
+    println!("{:#?}", file);
 }
 
-fn parse_args(args: &[String]) -> (&str, &str) {
-    return (&args[1], &args[2])
+#[derive(Debug)]
+struct Config {
+    query: String,
+    path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Too few arguments") //&format!("Expected two arguments. Got {}", args.len() -1 ));
+        }
+        return Ok(Self {
+            query: args[1].clone(),
+            path: args[2].clone(),
+        });
+    }
 }
