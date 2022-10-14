6c4b1250fd3a8cbb29c7609f546d81af0242fcb9
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-10-14 22:48:45 +0600

    chore: cargo format

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
+use minigrep::{run, Config};
 use std::{env, process};
-use minigrep::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-	let config = match Config::build(&args) {
-		Ok(t) => t,
-		Err(e) => {
-			println!("Error when parsing arguments. Error: {}", e);
-			process::exit(1);
-		}
-	};
+    let config = match Config::build(&args) {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Error when parsing arguments. Error: {}", e);
+            process::exit(1);
+        }
+    };
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.path);
     if let Err(e) = run(config) {
-		println!("Internal error, {e}");
-		process::exit(1);
-	};
+        println!("Internal error, {e}");
+        process::exit(1);
+    };
 }
