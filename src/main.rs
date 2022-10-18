6c8095d6882501000f7fa2ac7638ea88f736a8e7
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-10-18 16:07:39 +0600

    feat: print errors to stderror
    - use eprintln instead of println in error cases

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem with parsing arguments. Got error: {err}");
+        eprintln!("Problem with parsing arguments. Got error: {err}");
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.path);
     if let Err(e) = run(config) {
-        println!("Internal error, {e}");
+        eprintln!("Internal error, {e}");
         process::exit(1);
     };
 }
