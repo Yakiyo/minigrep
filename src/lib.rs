5baf32140341e50619f3ec52a1d1a283bb5b66ee
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-10-18 16:05:14 +0600

    feat: implement ignoring case

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs, env};
+use std::{env, error::Error, fs};
 
 #[derive(Debug)]
 pub struct Config {
@@ -12,38 +12,50 @@ impl Config {
         if args.len() < 3 {
             return Err("Too few arguments");
         }
-		let mut case = false;
-		if args.len() > 3 && args[3].to_lowercase() == "true" {
-			case = true;
-		} else if let Ok(t) = env::var("IGNORE_CASE") {
-			if t.to_lowercase() == "true" {
-				case = true;
-			}
-		}
-		println!("{}", case);
+        let mut case = false;
+        if args.len() > 3 {
+            if args[3].to_lowercase() == "true" { case = true; }
+        } else if let Ok(t) = env::var("IGNORE_CASE") {
+            if t.to_lowercase() == "true" {
+                case = true;
+            }
+        }
         return Ok(Self {
             query: args[1].clone(),
             path: args[2].clone(),
-			case_sensitive: case,
+            case_sensitive: case,
         });
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.path)?;
-
-    for line in search(&config.query, &contents) {
-        print!("{line}\n");
+    let contents = fs::read_to_string(&config.path)?;
+	let val = search(&config.query, &contents, &config.case_sensitive);
+	if val.is_empty() {
+		println!("No results matching query: {} found in file {}", &config.query, &config.path);
+	}
+    for line in search(&config.query, &contents, &config.case_sensitive) {
+        println!("{line}");
     }
 
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search<'a>(query: &str, contents: &'a str, case: &bool) -> Vec<&'a str> {
     let mut res: Vec<&str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            res.push(line);
+
+    if case == &(false) {
+        for line in contents.lines() {
+            if line.contains(query) {
+                res.push(line);
+            }
+        }
+    } else {
+        let query = query.to_lowercase();
+		for line in contents.lines() {
+            if line.to_lowercase().contains(&query) {
+                res.push(line);
+            }
         }
     }
     return res;
@@ -55,11 +67,29 @@ mod test {
     #[test]
     fn search_test() {
         let query = "duct";
+        let case = false;
         let contents = "\
-			Rust:\
-			safe, fast, productive.\
-			Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents, &case)
+        );
+    }
+	#[test]
+	fn search_test_insensitive() {
+        let query = "DuCt";
+        let case = true;
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents, &case)
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-	let config: Config = Config::build(&args).unwrap_or_else(|err| {
-		println!("Problem with parsing arguments. Got error: {err}");
-		process::exit(1);
-	});
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem with parsing arguments. Got error: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.path);
